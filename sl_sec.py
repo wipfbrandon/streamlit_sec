e951d7d8cea853855d41ef5edbd5392654ce2d87
wipfbrandon <77638067+wipfbrandon@users.noreply.github.com>
2023-03-19 19:58:27 -0500

    Create sl_sec.py

---
+++ sl_sec.py
@@ -0,0 +1,146 @@
+import streamlit as st
+import pandas as pd
+import requests
+
+cik_dict = {'Daktronics': '0000915779',
+            '3M Company': '0000066740',
+            'Union Pacific':'0000100885',
+            'Buckle Inc':'0000885245',
+            'ConAgra Foods Inc':'0000023217',
+            'Valmont Industries':'0000102729',
+            'Werner Enterprises':'0000793074',
+    }
+
+@st.cache_data
+def sec_api(cik):
+    headers = {'User-Agent': "pythonlearnin@gmail.com"}
+#COMPANY SUMMARY
+    comp_summ_list = []
+    url = f'https://data.sec.gov/submissions/CIK{cik}.json'
+    response = requests.get(url, headers=headers)
+    cik_name = response.json()['name']
+    comp_summ_list.append(cik_name)
+    sic_desc = response.json()['sicDescription']
+    comp_summ_list.append(sic_desc)
+    ticker = response.json()['tickers'][0]
+    comp_summ_list.append(ticker)
+    exchange = response.json()['exchanges'][0]
+    comp_summ_list.append(exchange)
+    fye = response.json()['fiscalYearEnd']
+    comp_summ_list.append(fye)
+    state_inc = response.json()['addresses']['business']['stateOrCountry']
+    comp_summ_list.append(state_inc)
+    city_inc = response.json()['addresses']['business']['city']
+    comp_summ_list.append(city_inc)
+
+#COMPANY FACTS
+    response = requests.get(f'https://data.sec.gov/api/xbrl/companyfacts/CIK{cik}.json', headers=headers)
+    # parsed = json.loads(response.text)
+    response_json = response.json()
+    entries = response_json['facts']['us-gaap']
+    df = pd.DataFrame(entries)
+    df_units = df.iloc[2:3] #ONLY WANT ROW 3, WHICH CONTAINS VALUES (I.E. "UNITS")
+
+    df_final = pd.DataFrame()
+    def scrub_json(data_list, entry_key):
+        df = pd.json_normalize(data_list[entry_key])
+        df['PERIOD'] = df['fy'].astype(str) + df['fp'].astype(str)
+        df = df.sort_values(by=['filed'], ascending=False)
+        df = df.dropna(subset=['frame'])
+        df = df[df['frame'] != '']
+        df = df.rename(columns={'end':'DATE'})
+        df = df.set_index(['DATE', 'PERIOD']).sort_index(ascending=False).drop_duplicates(subset=['accn'], keep='first')
+        df = df[['val']]
+        df = df.rename(columns={'val':f'{col}'})
+        return df
+
+    col_list = ('Revenues', 'SalesRevenueNet', 'RevenueFromContractWithCustomerExcludingAssessedTax', 'NetIncomeLoss',
+              'AssetsCurrent', 'LiabilitiesCurrent', 'AccountsReceivableNetCurrent', 'LiabilitiesAndStockholdersEquity',
+              'StockholdersEquity', 'NetIncomeLoss', 'Assets', 'InventoryNet', 'Liabilities', 'AccountsPayableCurrent',
+              'CommonStockValue', 'ComprehensiveIncomeNetOfTax', 'CostOfGoodsAndServicesSold',
+              'Depreciation', 'GrossProfit', 'HeldToMaturitySecurities', 'AvailableForSaleSecurities', 'NetCashProvidedByUsedInFinancingActivities',
+              'NetCashProvidedByUsedInInvestingActivities', 'NetCashProvidedByUsedInOperatingActivities', 'OperatingIncomeLoss',
+              'ProfitLoss')
+
+    df_final = pd.DataFrame()
+    y=0
+
+    for x, col in enumerate(df_units.columns):
+        test_list = df_units[col].tolist()[0]
+
+        if col in col_list:
+            try:
+                df_json = scrub_json(test_list, 'USD')
+                df_json = scrub_json(test_list, 'pure')
+                df_json = scrub_json(test_list, 'shares')
+                df_json = scrub_json(test_list, 'USD/shares')
+            except:
+                pass
+
+            if y == 0:
+                df_final = df_json
+                y =+ 1
+            else:
+                df_final = df_final.merge(df_json, how='left', left_index=True, right_index=True)
+
+    df_final = df_final.reset_index()
+
+    days_dict = {'FY':365.00, 'Q3':273.75, 'Q2':182.50, 'Q1':91.25}
+    def quarterly_financials(df_source, period='FY'):
+        days = days_dict[period]
+        df = df_source[df_source['PERIOD'].str.contains(f'{period}')]
+
+        try:
+            df['REVENUE_CUSTOM'] = df[['Revenues', 'SalesRevenueNet', 'RevenueFromContractWithCustomerExcludingAssessedTax']].max(axis=1)
+            df['REVENUE_CUSTOM'] = df[['Revenues', 'SalesRevenueNet']].max(axis=1)
+            df['REVENUE_CUSTOM'] = df[['Revenues']]
+            df['PROFIT_MARGIN'] = df['NetIncomeLoss'] / df['REVENUE_CUSTOM']
+            df['NET_INCOME_PCT_CHG'] = df['NetIncomeLoss'].pct_change(periods=-1)
+            df['CURRENT_RATIO'] = df['AssetsCurrent'] / df['LiabilitiesCurrent']
+            df['AR_DAYS'] = (df['AccountsReceivableNetCurrent'] / df['REVENUE_CUSTOM']) * days
+            df['LIABILITIES_CUSTOM'] = df['LiabilitiesAndStockholdersEquity'] - df['StockholdersEquity']
+        except:
+            pass
+        df = df.set_index(['DATE'])
+        df = df.dropna(axis=1, how='all')
+        return df
+
+    df_ye = quarterly_financials(df_final, 'FY')
+    # df_ye = df_ye[['Assets','LIABILITIES_CUSTOM', 'StockholdersEquity', 'AssetsCurrent',
+    #                'LiabilitiesCurrent','NetIncomeLoss','REVENUE_CUSTOM', 'PROFIT_MARGIN',
+    #                'NET_INCOME_PCT_CHG', 'CURRENT_RATIO','AR_DAYS']]
+
+    return df_ye, comp_summ_list
+
+#%% STREAMLIT OUTPUT
+add_selectbox = st.sidebar.selectbox(
+    'Select your Company',
+    (list(cik_dict.keys()))
+)
+
+cik_selected = cik_dict[add_selectbox]
+
+#%% SELECT SPECIFIC COMPANY
+result_set = sec_api(cik_selected)
+# df_selected_df = sec_api('0000066740')
+df_final = result_set[0]
+list_details = result_set[1]
+
+st.title('SEC Financials')
+st.sidebar.write(f'INDUSTRY: {list_details[1]}')
+st.sidebar.write(f'EXCHANGE: {list_details[3]}')
+st.sidebar.write(f'TICKER: {list_details[2]}')
+st.sidebar.write(f'FYE: {list_details[4][:2]}/{list_details[4][-2:]}')
+st.sidebar.write(f'OPERATIONS: {list_details[6]}, {list_details[5]}')
+
+f"""
+Financials Metrics for {add_selectbox}:
+"""
+st.dataframe(df_final)
+'''
+--
+AR Days
+'''
+st.bar_chart(df_final[['AR_DAYS']])
+'''--'''
+st.line_chart(df_final[['REVENUE_CUSTOM', 'NetIncomeLoss']])
