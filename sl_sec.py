75307b2523df184e0ca263c30522277ee68e9dc3
wipfbrandon <77638067+wipfbrandon@users.noreply.github.com>
2023-03-22 21:48:54 -0500

    Update sl_sec.py

--- sl_sec.py
+++ sl_sec.py
@@ -1,7 +1,24 @@
 import streamlit as st
 import pandas as pd
 import requests
+from datetime import date
 
+pd.options.mode.chained_assignment = None  # default='warn'
+curr_year = date.today().year
+lookback_years = 8
+
+period_list = []
+for x in range(0, lookback_years):
+    new_year = curr_year - x
+    frame_ye = f'CY{new_year}'
+    period_list.append(frame_ye)
+    for y in range(4, 0, -1):
+        frame_qe = f'CY{new_year}Q{y}'
+        frame_qe_i = f'CY{new_year}Q{y}I'
+        period_list.append(frame_qe)
+
+df_final = pd.DataFrame(index=period_list)
+		
 cik_dict = {'Daktronics': '0000915779',
             '3M Company': '0000066740',
             'Union Pacific':'0000100885',
@@ -39,53 +56,34 @@ def sec_api(cik):
 #COMPANY FACTS
     url =f'https://data.sec.gov/api/xbrl/companyfacts/CIK{cik}.json'
     r = requests.get(url, headers=headers)
-    
     df = pd.json_normalize(r.json()['facts']['us-gaap'])
     
-    df_final = pd.DataFrame()
     keep_list = ('Assets', 'Liabilities', 'StockholdersEquity', 'LiabilitiesAndStockholdersEquity',
                  'Revenues', 'SalesRevenueNet', 'RevenueFromContractWithCustomerExcludingAssessedTax')
     
-    y=0
-    for x, col in enumerate(df.columns):
-        col_name = col.split('.')[0]
-        col_type = col.split('.')[1]
-    
-        if col_name in keep_list:
-            if col_type == 'units':
-                temp_list = df[col].explode('TEMP')
-                df_temp = (pd.DataFrame(temp_list.apply(pd.Series)))
-    
-                df_temp['PERIOD'] = df_temp['fy'].astype(str) + df_temp['fp'].astype(str)
-                df_temp = df_temp.rename(columns={'end':'fin_end_date', 'val':f'{col_name}'})
-                df_temp_cy = df_temp[df_temp['frame'].astype(str).str.contains('CY') == True]
-                df_temp_cy['frame'] = df_temp_cy['frame'].str.replace('I', '')
-    
-                def fix_frame(frame, fp):
-                    if fp == 'FY':
-                        new_fp = 'Q4'
-                    else:
-                        new_fp = str(fp)
-                    if len(frame) < 7:
-                        new_frame = str(frame) + new_fp
-                    else:
-                        new_frame = frame
-                    return new_frame
-    
-                df_temp_cy['NEW_FRAME'] = df_temp_cy.apply(lambda row: fix_frame(row['frame'], row['fp']), axis=1)
-                df_temp_cy = df_temp_cy.drop_duplicates(subset=['NEW_FRAME'], keep='first')
-                df_temp_cy = df_temp_cy.set_index('NEW_FRAME')
-    
-                if y==0:
-                    df_final = df_temp_cy
-                    y=y+1
-                else:
-                    df_temp_cy = df_temp_cy[[f'{col_name}']]
-                    df_final = df_final.merge(df_temp_cy, how='left', left_index=True, right_index=True)
+	for x, col in enumerate(df.columns):
+			col_name = col.split('.')[0]
+			col_type = col.split('.')[1]
 
-    df_final = df_final.reset_index()
+			if col_name in keep_list:
+				if col_type == 'units':
+					temp_list = df[col].explode('TEMP')
+					df_temp = (pd.DataFrame(temp_list.apply(pd.Series)))
+					df_temp = df_temp.rename(columns={'val':f'{col_name}'})
+					df_temp['frame'] = df_temp['frame'].str.replace('I', '')
+					df_temp = df_temp.set_index('frame')
 
-    days_dict = {'FY':365.00, 'Q3':273.75, 'Q2':182.50, 'Q1':91.25}
+					if df_final.shape[1] == 0:
+						print(df_final.shape)
+						df_final = df_final.merge(df_temp, how='left', left_index=True, right_index=True)
+						df_final = df_final[['end','filed','fy','fp','form',f'{col_name}']]
+					else:
+						df_temp_cy = df_temp[[f'{col_name}']]
+						df_final = df_final.merge(df_temp, how='left', left_index=True, right_index=True)
+						
+    df_final = df_final.sort_index(ascending=False).reset_index()
+
+    days_dict = {'FY':365.00, 'Q4':91.25, 'Q3':91.25, 'Q2':91.25, 'Q1':91.25}
     def quarterly_financials(df_source, period='FY'):
         days = days_dict[period]
         df = df_source[df_source['PERIOD'].str.contains(f'{period}')]
@@ -105,7 +103,7 @@ def sec_api(cik):
         df = df.dropna(axis=1, how='all')
         return df
 
-    df_ye = quarterly_financials(df_final, 'FY')
+    df_ye = quarterly_financials(df_final, 'Q4')
     df_q3 = quarterly_financials(df_final, 'Q3')
     return df_ye, comp_summ_list, df_q3
 
